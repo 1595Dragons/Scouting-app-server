a7ecf6ec256763981770a2453615c1c4239dd826
Spud <jeffrypig23@gmail.com>
2018-01-10 16:10:39 -0800

    Add files via upload

---
+++ src/server.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import javax.bluetooth.RemoteDevice;
+import javax.bluetooth.UUID;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import javax.microedition.io.StreamConnectionNotifier;
+
+public class server {
+	// TODO: Update this to use the window, instead of the console
+	public void startServer() throws IOException {
+
+		// Create a UUID for SPP
+		UUID uuid = new UUID("1101", true);
+		// Create the servicve url
+		String connectionString = "btspp://localhost:" + uuid + ";name=Sample SPP Server";
+
+		// open server url
+		StreamConnectionNotifier streamConnNotifier = (StreamConnectionNotifier) Connector.open(connectionString);
+
+		// Wait for client connection
+		System.out.println("\nServer Started. Waiting for clients to connect…");
+		StreamConnection connection = streamConnNotifier.acceptAndOpen();
+
+		RemoteDevice dev = RemoteDevice.getRemoteDevice(connection);
+		System.out.println("Remote device address: " + dev.getBluetoothAddress());
+		System.out.println("Remote device name: " + dev.getFriendlyName(true));
+
+		// read string from spp client
+		InputStream inStream = connection.openInputStream();
+		BufferedReader bReader = new BufferedReader(new InputStreamReader(inStream));
+		String lineRead = bReader.readLine();
+		System.out.println(lineRead);
+
+		// send response to spp client
+		OutputStream outStream = connection.openOutputStream();
+		PrintWriter pWriter = new PrintWriter(new OutputStreamWriter(outStream));
+		pWriter.write("Response String from SPP Server\r\n");
+		pWriter.flush();
+		pWriter.close();
+
+		streamConnNotifier.close();
+
+	}
+}
