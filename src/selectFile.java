fd0ffa25bdd35f03c71ba8185e4d9b0294c3a48d
Spud <jeffrypig23@gmail.com>
2018-01-10 16:06:57 -0800

    Add files via upload

---
+++ src/selectFile.java
@@ -0,0 +1,63 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class selectFile {
+
+	static JFileChooser fileChooser = new JFileChooser();
+	static File file = new File(System.getProperty("user.dir") + "scouting_data.csv");
+
+	public static void CSV() {
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("CSV", "csv");
+		fileChooser.setFileFilter(filter);
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+
+		display display = new display();
+
+		display.feed.setText("Selecting CSV file...");
+		if (fileChooser.showOpenDialog(fileChooser) == JFileChooser.APPROVE_OPTION) {
+			// user selects a file
+			file = fileChooser.getSelectedFile().getAbsoluteFile();
+			display.feed.setText("File selected: " + file.toString());
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				display.feed.setText("Error occured while waiting!");
+			}
+			display.feed.setText("File selected: " + file.getName());
+		} else {
+			// Something else happened
+			display.feed.setText("Operation canceled");
+		}
+	}
+
+	public static boolean fileMissing() throws IOException {
+		return !file.isFile();
+	}
+
+	public static void makeFile() {
+		File csv = new File(System.getProperty("user.dir") + "scouting_data.csv");
+
+		FileWriter write = null;
+
+		try {
+			write = new FileWriter(csv);
+		} catch (IOException e) {
+		}
+
+		try {
+			write.append(
+					"Team number, hasAuto, autoSwitch, autoScale, teleSwitch, teleScale, cubeNumber, endClimb, endClimbAssist\n");
+			write.flush();
+			write.close();
+		} catch (IOException e) {
+
+		}
+		file = csv;
+	}
+
+}
