edf81cf3e2e30eb62ed17c645efa5bcff72d7e88
Spud <jeffrypig23@gmail.com>
2018-02-10 15:46:48 -0800

    More updates for content

--- src/takeData.java
+++ src/takeData.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
@@ -8,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSlider;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 
 public class takeData {
 
@@ -15,7 +17,7 @@ public class takeData {
 
 	JTextArea teamNumber;
 
-	JLabel teamHeader, autoHeader, teleHeader, space;
+	JLabel teamHeader, autoHeader, teleHeader;
 
 	JCheckBox hasAuto, autoSwitch, autoScale, teleSwitch, teleScale, endClimb, endClimbAssist;
 
@@ -29,9 +31,8 @@ public class takeData {
 		window = new JFrame("Data Collector");
 		layout = new GridBagConstraints();
 		teamHeader = new JLabel("Team to scout: ");
-		autoHeader = new JLabel("Autonomous");
-		teleHeader = new JLabel("TeleOp");
-		space = new JLabel("\n");
+		autoHeader = new JLabel("\nAutonomous");
+		teleHeader = new JLabel("\nTeleOp");
 		teamNumber = new JTextArea("Enter team number");
 		cubeNumber = new JSlider(0, 50, 0);
 		hasAuto = new JCheckBox("This team has an auto");
@@ -50,6 +51,11 @@ public class takeData {
 	public void addComponents() {
 		this.window.setLayout(new GridBagLayout());
 
+		this.cubeNumber.setMajorTickSpacing(10);
+		this.cubeNumber.setMinorTickSpacing(1);
+		this.cubeNumber.setPaintTicks(true);
+		this.cubeNumber.setPaintLabels(true);
+
 		this.layout.gridheight = 1;
 		this.layout.gridwidth = 1;
 		this.layout.gridx = 0;
@@ -60,18 +66,17 @@ public class takeData {
 		this.layout.gridwidth = 1;
 		this.layout.gridx = 1;
 		this.layout.gridy = 0;
-		this.window.add(this.teamHeader, this.layout);
+		this.window.add(this.teamNumber, this.layout);
 
-		JComponent[] objects = { this.space, this.autoHeader, this.hasAuto, this.autoSwitch, this.autoScale, this.space,
-				this.teleHeader, this.teleSwitch, this.teleScale, this.cubeNumber, this.endClimb, this.endClimbAssist,
-				this.space };
+		JComponent[] objects = { this.autoHeader, this.hasAuto, this.autoSwitch, this.autoScale, this.teleHeader,
+				this.teleSwitch, this.teleScale, this.cubeNumber, this.endClimb, this.endClimbAssist };
 
 		for (int i = 1; i < objects.length; i++) {
 			this.layout.gridheight = 1;
 			this.layout.gridwidth = 2;
 			this.layout.gridx = 0;
 			this.layout.gridy = i;
-			this.window.add(objects[i], this.layout);
+			this.window.add(objects[i - 1], this.layout);
 		}
 
 		this.layout.gridheight = 1;
@@ -90,20 +95,23 @@ public class takeData {
 		this.layout.gridwidth = 2;
 		this.layout.gridx = 0;
 		this.layout.gridy = 15;
-		this.window.add(space, this.layout);
-
-		this.layout.gridheight = 1;
-		this.layout.gridwidth = 2;
-		this.layout.gridx = 0;
-		this.layout.gridy = 16;
 		this.window.add(quit, this.layout);
 
+		resize();
 		this.window.pack();
 		this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	public void resize() {
+		// TODO: Resize everything
+	}
+
 	public void startGUI() {
+		this.window.setLocation(960, 540);
 		this.window.setVisible(true);
+	}
+	public void reset() {
+		// TODO: Reset everyghing (Submit/Cancel was clicked)
 	}
 
 }
