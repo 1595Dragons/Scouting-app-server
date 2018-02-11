d60f33e8fdc3db4d0fa98d9427d170f514b4fc84
Spud <jeffrypig23@gmail.com>
2018-02-10 16:55:55 -0800

    Added some functionality

--- src/takeData.java
+++ src/takeData.java
@@ -1,15 +1,19 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JSlider;
 import javax.swing.JTextArea;
-import javax.swing.SwingUtilities;
 
 public class takeData {
 
@@ -23,7 +27,7 @@ public class takeData {
 
 	JSlider cubeNumber;
 
-	JButton submit, cancel, quit;
+	JButton submit, cancel;
 
 	GridBagConstraints layout;
 
@@ -44,7 +48,6 @@ public class takeData {
 		endClimbAssist = new JCheckBox("This team can help others climb");
 		submit = new JButton("Submit");
 		cancel = new JButton("Cancel");
-		quit = new JButton("Quit");
 		addComponents();
 	}
 
@@ -71,11 +74,12 @@ public class takeData {
 		JComponent[] objects = { this.autoHeader, this.hasAuto, this.autoSwitch, this.autoScale, this.teleHeader,
 				this.teleSwitch, this.teleScale, this.cubeNumber, this.endClimb, this.endClimbAssist };
 
-		for (int i = 1; i < objects.length; i++) {
+		for (int i = 1; i <= objects.length; i++) {
 			this.layout.gridheight = 1;
 			this.layout.gridwidth = 2;
 			this.layout.gridx = 0;
 			this.layout.gridy = i;
+			objects[i - 1].setSize(objects[i - 1].getSize().width * 2, objects[i - 1].getSize().height * 2);
 			this.window.add(objects[i - 1], this.layout);
 		}
 
@@ -91,27 +95,60 @@ public class takeData {
 		this.layout.gridy = 14;
 		this.window.add(submit, this.layout);
 
-		this.layout.gridheight = 1;
-		this.layout.gridwidth = 2;
-		this.layout.gridx = 0;
-		this.layout.gridy = 15;
-		this.window.add(quit, this.layout);
-
-		resize();
-		this.window.pack();
-		this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
-	public void resize() {
-		// TODO: Resize everything
 	}
 
 	public void startGUI() {
-		this.window.setLocation(960, 540);
+		this.window.pack();
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		this.window.setLocation(d.width / 2 - this.window.getSize().width / 2,
+				d.height / 2 - this.window.getSize().height / 2);
+		readyListeners();
 		this.window.setVisible(true);
+		this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+
 	public void reset() {
 		// TODO: Reset everyghing (Submit/Cancel was clicked)
+	}
+
+	public void readyListeners() {
+		// TODO: Action listneners
+		this.cancel.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Object[] options = { "Yes", "No" };
+				int confirm = JOptionPane.showOptionDialog(null, "Are you sure you want to cancel?", "Cancel?",
+						JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[1]);
+				if (confirm == 0) {
+					reset();
+				}
+			}
+
+		});
+
+		this.submit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO: Check if a team number is entered, then write the data to the file
+				String data = "";
+				if (!takeData.this.teamNumber.getText().isEmpty()) {
+					try {
+						data = data + Integer.parseInt(takeData.this.teamNumber.getText());
+						System.out.print(data);
+					} catch (Exception e) {
+						takeData.this.teamHeader.setForeground(Color.RED);
+						return;
+					}
+				} else {
+					takeData.this.teamHeader.setForeground(Color.RED);
+					return;
+				}
+
+			}
+
+		});
 	}
 
 }
