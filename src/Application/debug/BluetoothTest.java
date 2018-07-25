6996df8ed6d64e2c06d624224b558eceb1ae3c4b
Spud <jeffrypig23@gmail.com>
2018-07-25 16:51:54 +0800

    Added a file for bluetooth stuff

---
+++ src/Application/debug/BluetoothTest.java
@@ -0,0 +1,23 @@
+package Application.debug;
+
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.LocalDevice;
+
+import Application.Debugger;
+
+public class BluetoothTest {
+
+	public static boolean isEnabled() {
+		return LocalDevice.isPowerOn();
+	}
+	
+	public static void changeMACDisplay() {
+		try {
+			InfoTest.MACAddressText.setText(LocalDevice.getLocalDevice().getBluetoothAddress().replaceAll("..(?!$)", "$0:").toUpperCase());
+		} catch (BluetoothStateException e) {
+			Debugger.d(BluetoothTest.class, e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+}
