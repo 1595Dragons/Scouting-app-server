d85a926bb2e83812deab2aa749f7dd2364f37129
Spud <jeffrypig23@gmail.com>
2018-07-26 15:08:28 +0800

    Added a placeholder class called Core for holding all the data

---
+++ src/Application/debug/Core.java
@@ -0,0 +1,8 @@
+package Application.debug;
+
+public class Core {
+
+	public static int BasicAutoValue, SwitchAutoValue, ScaleAutoValue, SwitchTeleValue, ScaleTeleValue, ExchangeTeleValue; // TODO: Climbing
+	public static String comments;
+	
+}
