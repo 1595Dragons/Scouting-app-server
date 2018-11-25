592f9d59feddf93b7fd4865e53074a943982d4b3
Spud <jeffrypig23@gmail.com>
2018-11-25 01:00:59 -0800

    Updated to the 2.1.2 snapshot, and moved to openJavaFX

--- src/javacode/Core/Bluetooth.java
+++ src/javacode/Core/Bluetooth.java
@@ -1,7 +1,14 @@
 package javacode.Core;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.LocalDevice;
+import javax.bluetooth.RemoteDevice;
+import javax.microedition.io.StreamConnection;
 
 import javacode.UI.MainPanel;
 
@@ -10,6 +17,7 @@ public class Bluetooth {
 	private LocalDevice server;
 	
 	
+
 	public boolean isEnabled() {
 		try {
 			Debugger.d(getClass(), "Bluetooth is on: " + Boolean.toString(LocalDevice.isPowerOn()));
@@ -30,6 +38,82 @@ public class Bluetooth {
 			}
 		} else {
 			throw new BluetoothStateException("Bluetooth is not turned on");
+		}
+	}
+
+	public class SSPServer extends Thread {
+
+		StreamConnection connection;
+
+		public SSPServer(StreamConnection connection) {
+			this.connection = connection;
+		}
+
+		// Wait for client connection
+		public void run() {
+
+			MainPanel debug = new MainPanel();
+
+			if (connection != null) {
+				synchronized (connection) {
+
+					RemoteDevice client = null;
+					try {
+						client = RemoteDevice.getRemoteDevice(connection);
+					} catch (IOException e) {
+						debug.log(e.getMessage() + "\n" + e.getStackTrace(), true);
+					}
+
+					if (client != null) {
+						InputStream inputStream = null;
+						try {
+							inputStream = connection.openInputStream();
+						} catch (IOException e) {
+							debug.log(e.getMessage() + "\n" + e.getStackTrace(), true);
+						}
+
+						if (inputStream != null) {
+
+							// Read the data from the stream
+							BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+							String lineRead = null;
+							try {
+								lineRead = bufferedReader.readLine();
+								if (lineRead == null) {
+									debug.log(String.format("%s (%s) has disconnected", client.getFriendlyName(false),
+											client.getBluetoothAddress()), false);
+								}
+							} catch (IOException e) {
+								debug.log(e.getMessage() + "\n" + e.getStackTrace(), true);
+							}
+
+							// TODO: Write to SQLite database
+
+							// TODO: Don't disconnect the device until requested
+							// But for now, just disconnect
+							try {
+								if (bufferedReader != null) {
+									bufferedReader.close();
+								}
+								if (inputStream != null) {
+									inputStream.close();
+								}
+								if (connection != null) {
+									connection.close();
+								}
+							} catch (IOException e) {
+								debug.log(e.getMessage() + "\n" + e.getStackTrace(), true);
+							}
+						} else {
+							return;
+						}
+					} else {
+						return;
+					}
+				}
+			} else {
+				debug.log("Bluetooth connection is null!\nCannot run on null stream", true);
+			}
 		}
 	}
 
--- src/javacode/ScoutingApp.java
+++ src/javacode/ScoutingApp.java
@@ -2,14 +2,24 @@ package javacode;
 
 import java.io.IOException;
 
-import javacode.OldCore.Debugger;
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.UUID;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import javax.microedition.io.StreamConnectionNotifier;
+
+import javacode.Core.Bluetooth;
+import javacode.Core.Debugger;
 import javacode.UI.MainPanel;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class ScoutingApp extends Application {
 
 	public static boolean debug;
+	private static boolean stopRequested = false;
 
 	@Override
 	public void start(Stage primaryWindow) {
@@ -20,6 +30,12 @@ public class ScoutingApp extends Application {
 			primaryWindow.setScene(mainPanel.loadMainPanel());
 			primaryWindow.setTitle("1595 Scouting app");
 			primaryWindow.setResizable(false);
+			primaryWindow.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				@Override
+				public void handle(WindowEvent event) {
+					stopRequested = true;
+				}
+			});
 			primaryWindow.show();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -27,6 +43,52 @@ public class ScoutingApp extends Application {
 		
 		
 		mainPanel.log("Loaded successfully", false);
+		
+		// Check if bluetooth is possible
+		Bluetooth bluetooth = new Bluetooth();
+		
+		if (bluetooth.isEnabled()) {
+			try {
+				mainPanel.setMacAddress(bluetooth.getMACAddress());
+				
+				// Create a UUID for SPP, and then create the URL
+				UUID uuid = new UUID("1101", true);
+				String connectionString = "btspp://localhost:" + uuid + ";name=DragonSPPServer";
+				mainPanel.log("Connection URL: " + connectionString, false);
+				
+				
+				StreamConnectionNotifier streamConnNotifier = null;
+				try {
+					streamConnNotifier = (StreamConnectionNotifier) Connector.open(connectionString);
+				} catch (IOException e) {
+					mainPanel.log(e.getMessage() + "\n" + e.getStackTrace(), true);
+				}
+				
+				// TODO: Run the following on a new thread
+				
+				while (!stopRequested) {
+					// Check if receiving a connection
+					// On connection, open it, and pass it to a newly created thread
+					StreamConnection connection = null;
+					try {
+						connection = streamConnNotifier.acceptAndOpen();
+						if (connection != null) {
+							// Start the SPP server for that device
+							new Thread(bluetooth.new SSPServer(connection)).start();
+						}
+					} catch (IOException e) {
+						mainPanel.log(e.getMessage() + "\n" + e.getStackTrace(), true);
+					}
+				}
+				
+				
+			} catch (BluetoothStateException e) {
+				mainPanel.log(e.getMessage() + "\n" + e.getStackTrace(), true);
+			}
+		} else {
+			mainPanel.log("Bluetooth not enabled", true);
+		}
+		
 
 	}
 
