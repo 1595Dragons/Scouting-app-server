ccc96f4485165415189f3d6d347865af94f4d470
Spud <jeffrypig23@gmail.com>
2018-12-24 13:50:54 +0100

    Created a custom datatype for match opmodes

---
+++ src/javacode/Core/Match.java
@@ -0,0 +1,45 @@
+package javacode.Core;
+
+public class Match {
+
+	Match.Autonomous[] autonomousData;
+	Match.TeleOp[] teleopData;
+	Match.Endgame[] endgameData;
+	
+	
+	public class Autonomous {
+		
+		String name;
+		DataType datatype;
+		Object count;
+		
+	}
+	
+	
+	public class TeleOp {
+		
+		String name;
+		DataType datatype;
+		Object count;
+		
+	}
+	
+	
+	public class Endgame {
+		
+		String name;
+		DataType datatype;
+		Object count;
+		
+	}
+	
+	
+	public enum DataType {
+		
+		Text,
+		Number,
+		Boolean;
+		
+	}
+	
+}
