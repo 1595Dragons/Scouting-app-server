21fd59819f3abfec011d87722eb5b2507d4ecccd
Spud <jeffrypig23@gmail.com>
2019-03-03 16:24:06 -0800

    macOS launch stuff

---
+++ Scouting app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java --module-path "3rd-party-libs/javafx-sdk-11.0.1/lib" --add-modules javafx.controls --add-exports=javafx.graphics/com.sun.javafx.util=ALL-UNNAMED --add-exports=javafx.base/com.sun.javafx.reflect=ALL-UNNAMED --add-exports=javafx.base/com.sun.javafx.beans=ALL-UNNAMED --add-exports=javafx.graphics/com.sun.glass.utils=ALL-UNNAMED --add-exports=javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED -jar Scouting-App-Server.jar
